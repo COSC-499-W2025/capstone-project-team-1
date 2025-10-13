ebccb5d6a06f5c83d438e382d59a780793be50e2
Ahmad Memon <122496580+ahmadmemon@users.noreply.github.com>
2025-10-12 22:30:08 -0700

    Revert "Added test cases"
    This reverts commit 57c08fb23d70387f1eeee06ecf5567fd820a6403.

--- tests/test_imports.py
+++
@@ -1,8 +0,0 @@
-def test_module_imports():
-    import artifactminer.tui.app as app
-    assert hasattr(app, "run")
-
-
-def test_app_class_exists():
-    import artifactminer.tui.app as app
-    assert hasattr(app, "ArtifactMinerTUI")
