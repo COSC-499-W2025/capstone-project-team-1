d29ea516317c020e05d4455effb21adda14c8040
van-cpu <ecrowl01@student.ubc.ca>
2025-10-19 15:48:15 -0700

    Created two tests for isGitRepo

--- tests/Repository-Intelligence-tests/test-Repository-Intelligence.py
+++ tests/Repository-Intelligence-tests/test-Repository-Intelligence.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from src.artifactminer.RepositoryIntelligence.repo_intelligence_main import RepoStats
+from src.artifactminer.RepositoryIntelligence.repo_intelligence_main import RepoStats,isGitRepo
 
 def test_create_RepoStats():
     mytestproject = RepoStats(project_name="test-project", primary_language="Python", is_collaborative=True)
@@ -19,3 +19,13 @@ def test_create_RepoStats_false():
     assert mytestproject_wrong.project_name != "test-project" #test to see if the name does not match the last test, and that the variable is being set correctly
     assert mytestproject_wrong.primary_language != "Python" #test to see if the language does not match the last test, and that the variable is being set correctly
     assert mytestproject_wrong.is_collaborative is not True #test to see if the project is being set correctly to show if it is collaborative or not.
+
+def test_isGitRepo(tmp_path):
+    repo = tmp_path/"projA"
+    (repo / ".git").mkdir(parents=True)
+    assert isGitRepo(repo) is True #checks that the git directory exists inside of the given path
+
+def test_isGitRepoFalse(tmp_path):
+    not_repo = tmp_path/"projB"
+    not_repo.mkdir()
+    assert isGitRepo(not_repo) is False #checks that the git directory does not exists inside of the given path
