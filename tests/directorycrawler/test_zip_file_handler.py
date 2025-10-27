fb0660b174fc8aaa2d6c2091be41011f66b3ba85
Brendan-James <35391207+Brendan-James@users.noreply.github.com>
2025-10-27 09:59:19 -0700

    fixed pytest compatibility

--- tests/directorycrawler/test_zip_file_handler.py
+++ tests/directorycrawler/test_zip_file_handler.py
@@ -7,5 +7,7 @@ from src.artifactminer.directorycrawler.zip_file_handler import process_zip
 dirname = os.path.dirname(__file__)
 zipfile = os.path.join(dirname,"mocks/mockdirectory_zip.zip")
 nonzipfile = os.path.join(dirname,"mocks/mockdirectory/mock.c")
-assert process_zip(zipfile, True) != None
-assert process_zip(nonzipfile) == None
+def test_read_zip():
+    assert process_zip(zipfile, True) != None
+def test_dont_read_nonzip():
+    assert process_zip(nonzipfile) == None
