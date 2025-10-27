de45d1c9d119e246cae2c8a0064c4bd52e1d1e06
Brendan-James <35391207+Brendan-James@users.noreply.github.com>
2025-10-26 19:28:37 -0700

    added basic zipfile handling

--- src/artifactminer/directorycrawler/zip_file_handler.py
+++ src/artifactminer/directorycrawler/zip_file_handler.py
@@ -1,14 +1,27 @@
 import zipfile
 
-ZIPFILEPATH = "/Users/nathanhelm/Code/Projects/capstone/capstone-project-team-1/tests/directorycrawler/mocks/mockdirectory_zip.zip"
-EXTRACTPATH = "" #TODO
-
-with zipfile.ZipFile(ZIPFILEPATH, 'r') as zip_ref:
+def process_zip(filePath: str, verbose: bool = False):
+    if not zipfile.is_zipfile(filePath):
+        print("Selected file is not a valid zipfile.") # we should presumably be using a more sophisticated method of error handling?
+        return
+    try:
+        with zipfile.ZipFile(filePath, 'r') as zip_ref:
+            if zip_ref.testzip() != None:
+                print("Zip contains malformed file: "+zip_ref.testzip())
+                return
+            
+            zipped_files = zip_ref.infolist()
+            if verbose:
+                for file in zipped_files:
+                    if not file.is_dir():
+                        print(file.filename)
+            return (zip_ref, zipped_files) # returns the contents of the zip and basic metadata on each file contained within the zip.
+            # zipped files can be later opened by calling zip_ref.open(zipped_file[N].filename where N is the index of file to be opened.  
     
-    print("unzipped files to be processed to os walk:")
-    zip_ref.printdir()
-    #later perform send content of zip using function below:
-    #zipf.extractall("unzipped_folder")
-
+    except zipfile.BadZipFile:
+        print("Selected zipfile is improper.")
+        return
+    except zipfile.LargeZipFile:
+        print("Selected zipfile is too large.")
     
 
