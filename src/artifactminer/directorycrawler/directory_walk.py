d9b9b022f023b61fa1afde2860212e9390079040
Nathan Helm <nathanhelm567@gmail.com>
2025-10-26 19:32:08 -0700

    functions added to directory_walk

--- src/artifactminer/directorycrawler/directory_walk.py
+++ src/artifactminer/directorycrawler/directory_walk.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
 
 #change this for a path that you choose
 CURRENTPATH = "/Users/nathanhelm/Code/Projects/capstone/capstone-project-team-1/tests/directorycrawler/mocks/mockdirectory"
 
 from .store_file_dict import StoreFileDict
+
+readableFileTypes = []
 
 store_file_dictionary = StoreFileDict()
 #storing files from mock folder to dictionary
@@ -22,7 +25,13 @@ def simple_directory_crawl():
                 if file != ".DS_Store": #check whether filename is valid
                     store_file_dictionary.add_to_dict(file, full_path) #key = filename, path = filepath
                 
+def is_file_readable(fileName):
+    print("yo")
 
+def get_file_from_TUI(p : Path):
+    CURRENTPATH = p
+
+    
 
 def print_files(file):
 
