fdadbd2e696f5af0d175c694b9c127efe973a025
Brendan-James <35391207+Brendan-James@users.noreply.github.com>
2025-10-27 10:37:12 -0700

    Adjusted error handling

--- src/artifactminer/directorycrawler/zip_file_handler.py
+++ src/artifactminer/directorycrawler/zip_file_handler.py
@@ -2,8 +2,7 @@ import zipfile
 
 def process_zip(filePath: str, verbose: bool = False):
     if not zipfile.is_zipfile(filePath):
-        print("Selected file is not a valid zipfile.") # we should presumably be using a more sophisticated method of error handling?
-        return
+        raise ValueError("Selected file is not a valid zipfile.")
     try:
         with zipfile.ZipFile(filePath, 'r') as zip_ref:
             if zip_ref.testzip() != None:
@@ -18,10 +17,9 @@ def process_zip(filePath: str, verbose: bool = False):
             return (zip_ref, zipped_files) # returns the contents of the zip and basic metadata on each file contained within the zip.
             # zipped files can be later opened by calling zip_ref.open(zipped_file[N].filename where N is the index of file to be opened.  
     
-    except zipfile.BadZipFile:
-        print("Selected zipfile is improper.")
-        return
-    except zipfile.LargeZipFile:
-        print("Selected zipfile is too large.")
+    except zipfile.BadZipFile as ziperror:
+        raise ziperror
+    except zipfile.LargeZipFile as ziperror:
+        raise ziperror
     
 
