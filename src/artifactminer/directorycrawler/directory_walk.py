fecaa259d35761bd0526b8f38d6074d4e6c41e42
Nathan Helm <nathanhelm567@gmail.com>
2025-11-02 12:14:39 -0800

    include/exclude files from mock directory

--- src/artifactminer/directorycrawler/directory_walk.py
+++ src/artifactminer/directorycrawler/directory_walk.py
@@ -6,14 +6,18 @@ CURRENTPATH = "/Users/nathanhelm/Code/Projects/capstone/capstone-project-team-1/
 
 from .store_file_dict import StoreFileDict
 
-readableFileTypes = []
+readableFileTypes = [] #TODO
 
 ignoredFileNames = [".DS_Store"] #file name not file extension
 ignoredExtensions = [".tmp", ".log", ".bak", ".gitignore"]
 
+userExcludeFileName = []    #["excluded_file.py"] #user's file that will be excluded
+userKeepFileName = []    #["include_file.log"] #even though its 'log' the user has specifically asked us to use it
+userIncludeAllFiles = False 
+
 store_file_dictionary = StoreFileDict()
 #storing files from mock folder to dictionary
-def simple_directory_crawl(): 
+def crawl_directory(): 
     if os.path.exists(CURRENTPATH) == False:
         print("path does not exist")
         return
@@ -25,12 +29,17 @@ def simple_directory_crawl():
             for file in files: 
                
                 full_path = os.path.join(root, file)
-                if is_file_ignored(file) == False: #check whether filename is valid
-                    print("file name: ", file ," is ignored")
+                if file in userExcludeFileName: #user 
                     continue
-                if is_file_readable(full_path) == False:
-                    print("file name: ", file, " is not readble")
-                    continue
+                if file not in userKeepFileName: #if file in user file name skip other functions
+                    if is_file_ignored(file) == False: #check whether filename is valid
+                        print("file name: ", file ," is ignored")
+                        continue
+                    if is_file_readable(full_path) == False:
+                        print("file name: ", file, " is not readble")
+                        continue
+                else:
+                    print("user file include name: ", file)
                 print_files(file) #print files
                 store_file_dictionary.add_to_dict(file, full_path) #key = filename, path = filepath
                 
@@ -57,17 +66,21 @@ def is_file_ignored(file_name: str) -> bool:
         return False
     
     # Might add this filter for later.
-    # If readableFileTypes is not empty, enforce filter
-    #if readableFileTypes and ext.lower() not in readableFileTypes:
-    #   return False
+    #If readableFileTypes is not empty, enforce filter
+    if readableFileTypes and ext.lower() not in readableFileTypes:
+        return False
     return True
 
 
+def user_keep_file(fileName):
+    userKeepFileName.append(fileName)
 
+def user_exclude_file(fileName):
+    userExcludeFileName.append(fileName)
 
 def print_files(file):
 
     print("\n>", file)
 
 
-simple_directory_crawl()
+crawl_directory()
--- tests/directorycrawler/test_directory_walk.py
+++ tests/directorycrawler/test_directory_walk.py
@@ -5,13 +5,29 @@ import copy
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))) #goto project directory
 
 from src.artifactminer.directorycrawler.store_file_dict import StoreFileDict #getting class from store file dict
-from src.artifactminer.directorycrawler.directory_walk import simple_directory_crawl
-
+from src.artifactminer.directorycrawler.directory_walk import user_keep_file, user_exclude_file, crawl_directory
 a = StoreFileDict() #single instance
 
 def test_gathered_files_from_oswalk():
-    simple_directory_crawl() 
+    crawl_directory() 
     assert a.get_dict_len() == 4 #assuming we are getting all files from mock directory
     a.remove_all_dict() #remove all elements from dictionary
 
-    
+def test_include_file_user_setting():
+
+    # two files that won't be ignored
+    user_keep_file("bugbomb.gitignore")
+    user_keep_file("bugbomb.log")
+    crawl_directory() 
+    assert a.get_dict_len() == 6
+    a.remove_all_dict()
+
+def test_exclude_file_user_setting():
+    user_keep_file("bugbomb.gitignore")
+    user_keep_file("bugbomb.log")
+    user_exclude_file("bugbomb.gitignore")
+    user_exclude_file("bugbomb.log")
+    crawl_directory()
+    assert a.get_dict_len() == 4
+    a.remove_all_dict()
+
