8506c40a6c221cace6811b0e85664874450ae7ed
ahmad <ahmadamemon02@gmail.com>
2025-11-01 17:44:59 -0700

    UI fixes done and did code refactoring to have seperate files for sperate steps in the process.

--- src/artifactminer/tui/screens/__init__.py
+++ src/artifactminer/tui/screens/__init__.py
@@ -1,1 +1,11 @@
+from .consent import ConsentScreen
+from .list_contents import ListContentsScreen
+from .upload import UploadScreen
+from .welcome import WelcomeScreen
 
+__all__ = [
+    "ConsentScreen",
+    "ListContentsScreen",
+    "UploadScreen",
+    "WelcomeScreen",
+]
