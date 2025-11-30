12d17f48ce85825e08bf26d0e964eab6ab01b31b
Nathan Helm <nathanhelm567@gmail.com>
2025-11-30 01:08:36 -0800

    updated demo

--- tests/artifactminer_demo/test_artifactminer_demo.py
+++ tests/artifactminer_demo/test_artifactminer_demo.py
@@ -1,7 +1,15 @@
+from sqlalchemy import text
 from artifactminer.artifactminer_demo.artifactminer_demo import run_demo
 import artifactminer.artifactminer_demo.artifactminer_demo as demo
+from artifactminer.db.database import SessionLocal
 def test_artifact_demo():
     demo.ZIPPATH = "/Users/nathanhelm/Code/Projects/capstone/capstone-project-team-1/tests/directorycrawler/mocks/mockdirectory-git.zip"
     demo.EMAIL = "ecrowl01@student.ubc.ca"
     demo.run_demo()
+    db = SessionLocal()#create a new session (copied from test_repo_intelligence)
+    sql ="SELECT * FROM repo_stats;"
+    res = db.execute(text(sql)).all()
+    objects = [dict(row._mapping) for row in res]
+    assert objects[0]["project_name"] == "mockdirectory-git" 
+    assert objects[1]["project_name"] == "mock-git_2"
     
