6b41fb9eaf0a56624e43cb40db3d25925b45b8a9
Nathan Helm <nathanhelm567@gmail.com>
2026-01-11 22:40:29 -0800

    add according changes

--- tests/directorycrawler/test_crawler_api.py
+++ tests/directorycrawler/test_crawler_api.py
@@ -1,37 +1,21 @@
-import sys
-import os
 from pathlib import Path
 import requests
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))) #goto project directory
 
-from src.artifactminer.directorycrawler.store_file_dict import StoreFileDict #getting class from store file dict
-from src.artifactminer.directorycrawler.directory_walk import crawl_directory,user_keep_file, user_exclude_file, CURRENTPATH, userExcludeFileName, userKeepFileName #getting function from directory walk
-import src.artifactminer.directorycrawler.directory_walk as dw
-a = StoreFileDict() #single instance
+from artifactminer.directorycrawler.store_file_dict import StoreFileDict
 
+a = StoreFileDict()
 
-# YOU MUST RUN > uv run api in order for this to work
+def test_api_call():
+    zip_path = Path(__file__).parent / "mocks" / "mockdirectory_zip_test_duplicate.zip"
+    response = post_file(zip_path)
 
+    assert response.status_code == 200
 
-def test_api_call(): 
-
-    response = post_file("/Users/nathanhelm/Code/Projects/capstone/capstone-project-team-1/tests/directorycrawler/mocks/mockdirectory_zip_test_duplicate.zip","mockdirectory_zip_test_duplicate.zip")
-    
-    print(response.status_code)
-    print(response.json())
-
-    assert 0 == 0
-
-def post_file(path : str,fileName : str):
-    
+def post_file(zip_path: Path):
     url = "http://localhost:8000/upload"
-    zip_path = path
-    with open(zip_path, "rb") as f:
-        files = {
-            "file": (fileName, f, "application/zip")
-        }
-        response = requests.post(url, files=files)
-    return response
 
+    with zip_path.open("rb") as f:
+        files = {"file": (zip_path.name, f, "application/zip")}
+        return requests.post(url, files=files)
 
   
