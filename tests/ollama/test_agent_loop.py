ba1d92a18c6b612a7efabb914ecb314edcea53a3
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2026-01-19 10:31:17 -0800

    add agent loop tests

---
+++ tests/ollama/test_agent_loop.py
@@ -0,0 +1,8 @@
+OLLAMA_MODELS: List[str] = [
+    "gemma3:4b",  # Does not support tool calling
+    "gemma3:1b",  # Does not support tool calling
+    "llama3.2:1b",
+    "llama3.2:3b",
+    "qwen3:1.7b",  # Thinking and non thinking both
+    "qwen3:4b-q4_K_M",
+]
