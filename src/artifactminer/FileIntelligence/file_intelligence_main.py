45f8d5338ef99ba51681fcce64579e355cc968fc
van-cpu <113214673+van-cpu@users.noreply.github.com>
2026-01-27 19:01:08 -0800

    Created initial file_intelligence_main.py file, with documentation outline of file purpose

---
+++ src/artifactminer/FileIntelligence/file_intelligence_main.py
@@ -0,0 +1,4 @@
+#The main goal of this file is to achieve full file analysis given a path to a zip folder
+# This would mean being able to analyze a PDF for example for any essential information 
+# that could be used in a resume. Primarily done using already made AI analysis functions, but
+# also using basic string matching in order to comply with non-AI file analysis.
