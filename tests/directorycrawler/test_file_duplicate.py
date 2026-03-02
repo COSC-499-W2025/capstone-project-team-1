e98fa4511616683f998f0a9f4827b4855e3d8991
Nathan Helm <nathanhelm567@gmail.com>
2026-03-01 19:36:33 -0800

    changes have been addressed and made

--- tests/directorycrawler/test_file_duplicate.py
+++ tests/directorycrawler/test_file_duplicate.py
@@ -1,18 +1,16 @@
 import sys
 import os
 from pathlib import Path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))) #goto project directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from src.artifactminer.directorycrawler.store_file_dict import StoreFileDict #getting class from store file dict
-from src.artifactminer.directorycrawler.directory_walk import crawl_directory, print_values_in_dict  #getting function from directory walk
+from src.artifactminer.directorycrawler.directory_walk import crawl_directory, print_values_in_dict, store_file_dictionary
 import src.artifactminer.directorycrawler.directory_walk as dw
-a = StoreFileDict() #single instance
 
 def test_check_file_duplicate():
-    a.remove_all_dict()
-    dw.MOCKNAME = "mockdirectory2" #change to a new mock directory
+    store_file_dictionary.remove_all_dict()
+    dw.MOCKNAME = "mockdirectory2"
     dw.update_path()
-    crawl_directory()
+    files_dict, dirs_list = crawl_directory()
     print_values_in_dict()
-    assert a.get_dict_len() == 2 # 1 is readme text file, 2 is c file, other c file should be removed!
+    assert len(files_dict) == 2
 
