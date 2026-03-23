a57a389d9855558fcf236f4ffcbc8aa19c709356
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2026-03-23 00:04:30 -0700

    Remove unused exports from cloud-ai index

--- opentui-react-exp/src/components/cloud-ai/index.ts
+++ opentui-react-exp/src/components/cloud-ai/index.ts
@@ -1,7 +1,3 @@
 export { CloudAuth } from "./CloudAuth";
 export { CloudFlow } from "./CloudFlow";
-export { CopilotLogin } from "./CopilotLogin";
-export { CLOUD_MODELS, DEFAULT_MODEL_ID, type ModelOption } from "./ModelPicker";
-export { ModelList, type ModelListResult } from "./ModelList";
-export { SnakeGame } from "./SnakeGame";
 export { SnakeWithProgress } from "./SnakeWithProgress";
