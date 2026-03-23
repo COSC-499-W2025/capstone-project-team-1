9419266584fce2b1d85008de6fedd106daff65ff
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2026-03-22 22:38:42 -0700

    Remove GenerationProgress export from cloud-ai module
    Update exports to reflect removal of the GenerationProgress component.

--- opentui-react-exp/src/components/cloud-ai/index.ts
+++ opentui-react-exp/src/components/cloud-ai/index.ts
@@ -3,6 +3,5 @@ export { CloudFlow } from "./CloudFlow";
 export { CopilotLogin } from "./CopilotLogin";
 export { CLOUD_MODELS, DEFAULT_MODEL_ID, type ModelOption } from "./ModelPicker";
 export { ModelList, type ModelListResult } from "./ModelList";
-export { GenerationProgress } from "./GenerationProgress";
 export { SnakeGame } from "./SnakeGame";
 export { SnakeWithProgress } from "./SnakeWithProgress";
